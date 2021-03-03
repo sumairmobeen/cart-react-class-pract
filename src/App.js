730eb59c93a1e4291bdcc1d2e18f47846b4d7ef9
nomanali630 <hussainkhaan78@gmail.com>
2021-03-03 18:32:08 +0500

    update-1

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import("./Dashboard");
 
 function App() {
 
-  
+
   const [cart, setCart] = useState([]);
   const [item, setItem] = useState([{
     CarName: "Audi",
     amount: 12000000,
-    quantity: 0,
+    quantity: 1,
     image:
       "https://freepngimg.com/thumb/car/11-2-car-picture.png",
     id: 7001,
@@ -20,7 +20,7 @@ function App() {
   {
     CarName: "Suzuki",
     amount: 1720000,
-    quantity: 0,
+    quantity: 1,
     image:
       "http://www.pngmart.com/files/10/Suzuki-Car-PNG-Photos.png",
     id: 8532,
@@ -29,14 +29,82 @@ function App() {
   {
     CarName: "ISUZU",
     amount: 4920000,
-    quantity: 0,
+    quantity: 1,
     image:
       "http://pngimg.com/uploads/isuzu/isuzu_PNG32.png",
     id: 7632,
     lessThanZero: false,
   },
   ]);
-  // function AddToCart(value) {
+
+  // function AddCart(value) {
+  //   setCart([...cart,value])
+
+  // return console.log(value)
+
+
+  function AddItem(product) {
+    const exist = AddItem.find((eachproduct) => eachproduct.id === product.id)
+    if (exist === undefined) {
+      setCart([...cart, product])
+    }
+
+  }
+
+
+
+  return (
+
+
+    <div className="main">
+
+      {item.map((value, index) => (
+        <div key={index}>
+
+          <img src={value.image}></img>
+          <p>{value.CarName}</p>
+          <p>{value.amount}</p>
+          <p>{value.quantit}</p>
+          <p>{value.id}</p>
+
+          <button onClick={() => {
+            AddItem(product)
+          }}>Add To Cart</button>
+
+        </div>
+
+      ))};
+
+      <div>
+
+      {cart.map((value, index) => (
+        <div key={index}>
+
+          <img src={value.image}></img>
+          <p>{value.CarName}</p>
+          <p>{value.amount}</p>
+          <p>{value.quantit}</p>
+          <p>{value.id}</p>
+
+         
+        </div>
+
+      ))};
+
+      </div>
+
+    </div>
+  )
+}
+export default App;
+
+
+
+
+
+
+
+// function AddToCart(value) {
   //   const exist = cart.find((x) => x._id === value._id)
   //   if (exist) {
   //     setCart(
@@ -57,50 +125,3 @@ function App() {
   // }
 
 
-
-    function AddCart(value) {
-      setCart([cart[value]])
-
-      return console.log(value)
-
-      
-
-
-
-
-    }
-
-
-
-  // function Del() {
-
-  //     setItem(item - 1)
-  //   };
-
-  return (
-   
-
-    <div className="main">
-      <h2>{cart}</h2>
-
-      {item.map((value, index) => (
-        <div key={index}>
-
-          <img src={value.image}></img>
-          <p>{value.CarName}</p>
-          <p>{value.amount}</p>
-          <p>{value.quantit}</p>
-          <p>{value.id}</p>
-
-          <button onClick={()=>{
-            AddCart(value)
-          }}>Add To Cart</button>
-
-        </div>
-
-      ))};
-
-    </div>
-  )
-}
-export default App;
